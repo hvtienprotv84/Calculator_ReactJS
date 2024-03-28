9c704b584c9a45f6054dbb593a9b8b0d7bf58b84
Huỳnh Vĩnh Tiến <dmx031117@gmail.om>
2024-03-28 20:23:13 +0700

    Updates

---
+++ precache-manifest.38689833a97edc0b4aa4776bf7b671df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321e466fa8be7a6cb8f09a91aa678f0c",
+    "url": "/Calculator_ReactJS/index.html"
+  },
+  {
+    "revision": "c04d69c21f6cab0a90f6",
+    "url": "/Calculator_ReactJS/static/css/main.f8277b9e.chunk.css"
+  },
+  {
+    "revision": "55d9828ca1827bb8cae8",
+    "url": "/Calculator_ReactJS/static/js/2.a5d8ed2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator_ReactJS/static/js/2.a5d8ed2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04d69c21f6cab0a90f6",
+    "url": "/Calculator_ReactJS/static/js/main.7d16501f.chunk.js"
+  },
+  {
+    "revision": "5cedad2ec2ae4303b8eb",
+    "url": "/Calculator_ReactJS/static/js/runtime-main.4c9d4bdb.js"
+  }
+]);
